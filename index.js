37fae06348afcd8c8575d60f871c1a9d37ff6e74
Courtney Garcia <wavesofgrace3@gmail.com>
2022-04-06 15:23:33 -0700

    commit tree 225adc8adc787780449363c8ca47f770a60a8712

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let message = [];
+    for (let i=0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return message;
+} 
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer)
+        integer--;
+    }
+}
+
+countDown(4)
